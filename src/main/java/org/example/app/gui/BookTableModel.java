4d6b08f19f673ce80d871ded2c6c76639c6f03bc
mauriziosodano <maurizio.sodano@gmail.com>
2022-04-01 16:54:31 +0200

    added tableModel and JTable

---
+++ src/main/java/org/example/app/gui/BookTableModel.java
@@ -0,0 +1,49 @@
+package org.example.app.gui;
+
+import org.example.app.model.Book;
+
+import javax.swing.table.AbstractTableModel;
+import java.util.List;
+
+public class BookTableModel extends AbstractTableModel {
+    private List<Book> bookList = List.of(new Book("title1", "author1"), new Book("title2", "author2"));
+
+    @Override
+    public int getRowCount() {
+        return bookList.size();
+    }
+
+    @Override
+    public int getColumnCount() {
+        return 3;
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        var book = bookList.get(rowIndex);
+        switch (columnIndex) {
+            case 0:
+                return book.getId();
+            case 1:
+                return book.getAuthor();
+            case 2:
+                return book.getTitle();
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public String getColumnName(int column) {
+
+        switch (column) {
+            case 0:
+                return "ID";
+            case 1:
+                return "Author";
+            case 2:
+                return "Title";
+        }
+        return "";
+    }
+}
--- src/main/java/org/example/app/gui/ViewBooksPanel.java
+++ src/main/java/org/example/app/gui/ViewBooksPanel.java
@@ -5,7 +5,21 @@ import java.awt.*;
 
 public class ViewBooksPanel extends JPanel {
 
+    private BookTableModel tableModel;
+    private JTable table;
+
     public ViewBooksPanel() {
+        tableModel=new BookTableModel();
+        table=new JTable(tableModel);
+
         setBackground(Color.green);
+
+        setLayout(new BorderLayout());
+
+        JScrollPane scrollPane=new JScrollPane(table);
+
+        add(scrollPane, BorderLayout.CENTER);
+
+
     }
 }
